56d570bc0fcabe4720a491f15a0933cc8b42f97a
donghyukkil <asterism90@gmail.com>
2023-08-17 19:46:46 +0900

    feat: client 요청 저장 기능 추가

--- controllers/texts.controller.js
+++ controllers/texts.controller.js
@@ -1,11 +1,62 @@
 const createError = require("http-errors");
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 
+const Text = require("../models/Text");
+const User = require("../models/User");
+
+const { isIdValid, isUserIdValid } = require("../utils");
+
 exports.createTexts = async (req, res, next) => {
+  const { userId } = req.params;
+  const { content } = req.body;
+
+  if (!isUserIdValid(userId)) {
+    next(createError(StatusCodes.BAD_REQUEST, ReasonPhrases.BAD_REQUEST));
+
+    return;
+  }
+
+  if (typeof content !== "string") {
+    next(createError(StatusCodes.BAD_REQUEST, ReasonPhrases.BAD_REQUEST));
+
+    return;
+  }
+
   try {
-    res.json({
-      OK: "OK",
-    });
+    const user = await User.findOne({ email: userId });
+
+    if (!user) {
+      next(createError(StatusCodes.NOT_FOUND, ReasonPhrases.NOT_FOUND));
+
+      return;
+    }
+
+    const saveText = async () => {
+      try {
+        const text = new Text({
+          userId: user._id,
+          content,
+        });
+
+        await text.save();
+
+        res.json({
+          status: 201,
+          message: "Text created successfully",
+          data: {
+            text,
+          },
+        });
+      } catch (error) {
+        next(
+          createError(
+            StatusCodes.INTERNAL_SERVER_ERROR,
+            ReasonPhrases.INTERNAL_SERVER_ERROR,
+          ),
+        );
+      }
+    };
+    saveText();
   } catch (error) {
     next(
       createError(
