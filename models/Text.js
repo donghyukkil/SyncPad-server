07e3c1e1b478625fa0584574e9f235b3abc9851e
donghyukkil <asterism90@gmail.com>
2023-08-17 17:31:15 +0900

    feat: Text schema 수정

--- models/Text.js
+++ models/Text.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const textSchema = new Schema({
-  userEmail: { type: String, ref: "User", required: true },
+  userId: { type: String, ref: "User", required: true },
   content: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
