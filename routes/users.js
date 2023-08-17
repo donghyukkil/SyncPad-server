8e5550f195325f3539d8c2e4f6fd12ed1c93bb9c
donghyukkil <asterism90@gmail.com>
2023-08-17 17:51:59 +0900

    feat: texts controller 라우터 추가

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 const loginController = require("../controllers/login.controller");
+const textsController = require("../controllers/texts.controller");
 
 router.post("/", loginController.post);
+
+router.post("/:userId/texts", textsController.createTexts);
 
 module.exports = router;
