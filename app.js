f928e59c9493bfaef624416b5cf4da78ec1f7bae
donghyukkil <asterism90@gmail.com>
2023-08-19 09:29:52 +0900

    fix: body parsing 설정 업데이트: 10mb 제한 추가

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ const connectToDatabase = async () => {
 connectToDatabase();
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cookieParser());
