64d37ad5c445cbef18f9efdfe108aab394448dbd
donghyukkil <asterism90@gmail.com>
2023-08-28 17:52:46 +0900

    fix: textSchema content type 변경

--- models/Text.js
+++ models/Text.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 
 const textSchema = new Schema({
   userId: { type: String, ref: "User", required: true },
-  content: { type: String, required: true },
+  content: { type: Array, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
