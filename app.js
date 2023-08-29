8aaa82cff4142200bd27070e07d41e9b02e03c12
donghyukkil <asterism90@gmail.com>
2023-08-29 14:09:08 +0900

    fix: 개발 환경에서만 dotenv 패키지 임포트하도록 수정

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-require("dotenv").config();
+if (process.env.NODE_ENV === "development") {
+  const dotenv = require("dotenv");
+  dotenv.config();
+}
 
 const path = require("path");
 const createError = require("http-errors");
