9b4b4847c6ae684919d9414fb7f75b8a0e234895
donghyukkil <asterism90@gmail.com>
2023-11-16 16:11:50 +0900

    feat: RoomSchema에 content field 추가

--- models/Room.js
+++ models/Room.js
@@ -6,6 +6,7 @@ const RoomSchema = Schema({
   userId: { type: String, ref: "User", required: true },
   textId: String,
   roomName: String,
+  content: Array,
 });
 
 module.exports = mongoose.model("Room", RoomSchema);
