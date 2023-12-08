8c17e29b7f4a639196ce35dfe8ecdd999c62b965
donghyukkil <asterism90@gmail.com>
2023-12-08 10:07:24 +0900

    feat: verifyToken 생성 및 적용

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,10 @@ const loginController = require("../controllers/login.controller");
 const textsController = require("../controllers/texts.controller");
 const roomController = require("../controllers/room.controller");
 
-router.post("/", loginController.post);
+const verifyToken = require("../middlewares/verifyToken");
+const verifyCookie = require("../middlewares/verifyCookie");
+
+router.post("/", verifyToken, loginController.post);
 
 router.post("/logout", loginController.logout);
 
@@ -14,10 +17,18 @@ router.post("/:userId/create", textsController.createText);
 router.post("/:userId/upload", textsController.uploadText);
 router.get("/:userId/texts", textsController.getTexts);
 router.put("/:userId/texts/:textId", textsController.putText);
-router.delete("/:userId/texts/:textId", textsController.deleteText);
+router.delete(
+  "/:userId/texts/:textId",
+  verifyCookie,
+  textsController.deleteText,
+);
 
 router.post("/:userId/createRoom", roomController.createRoom);
 router.get("/:userId/getRooms", roomController.getroom);
-router.delete("/:userId/deleteRooms/:roomId", roomController.deleteRoom);
+router.delete(
+  "/:userId/deleteRooms/:roomId",
+  verifyCookie,
+  roomController.deleteRoom,
+);
 
 module.exports = router;
